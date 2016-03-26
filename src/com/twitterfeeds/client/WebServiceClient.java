2e5d20db9117259124279457e46e0c1ba29fd398
BhagyashreeVK <bvkulkarni2512@gmail.com>
2016-03-26 16:21:52 -0400

    Web service client that consumes Sensor Twitter Feeds Webservice

---
+++ src/com/twitterfeeds/client/WebServiceClient.java
@@ -0,0 +1,36 @@
+package com.twitterfeeds.client;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientResponse;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+
+public class WebServiceClient {
+
+	
+	private void getTweetsResponse() {
+		try {
+ 
+			Client client = Client.create();
+			WebResource webResource = client.resource("http://localhost:8080/SensorTwitterFeeds/twitterfeeds/getTweets?count=2");
+			ClientResponse response = webResource.get(ClientResponse.class);
+			if (response.getStatus() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+			}
+ 
+			String output = response.getEntity().toString();
+			System.out.println("\n============getTweetsResponse============");
+			System.out.println(output);
+ 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+	
+		WebServiceClient wsClient = new WebServiceClient();
+		wsClient.getTweetsResponse();
+	}
+}
